0be87fb266893620182972bcfcd9d22b4c3cecb7
Omar Ali Sheikh <omsh@atp.dk>
2022-10-28 15:37:16 +0200

    Improve commit message on deploy.

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -3,14 +3,18 @@ import { execa } from "execa";
 
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
+    var { stdout  } = await execa("git", ["rev-parse", "HEAD"])
+    const currentCommitSHA = stdout
+    var { stdout  } = await execa("git", ["log", "-1", "--pretty=%B"])
+    const currentCommitMsg = stdout.trim();
+    const newGitMessage = `New deployment to GitHub Pages.\n\nBased on '${currentCommitMsg}'\nSHA: ${currentCommitSHA}`
     await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
     const folderName = "dist";
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", newGitMessage]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
