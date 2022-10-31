f75a292cf781ee82d5d995b3ecb6bd6341548ce9
Omar Ali Sheikh <omsh@atp.dk>
2022-10-31 11:16:42 +0100

    Adjust drawing style.

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,13 @@ import VueDrawingCanvas from "vue-drawing-canvas";
   <h1>Drawing Canvas</h1>
   <vue-drawing-canvas
     ref="VueCanvasDrawing"
-    width="600"
-    height="400"
+    :width="600"
+    :height="400"
     background-color="#efefef"
+    stroke-type="dash"
+    line-cap="round"
+    line-join="round"
+    :lineWidth="3"
   />
 </template>
 
