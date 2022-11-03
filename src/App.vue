ced1410cad9edf5a9697c763dcf8fe82f5d8cf6f
Omar Ali Sheikh <omsh@atp.dk>
2022-11-03 11:33:04 +0100

    Ability to dynamically add new lines.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
-import DrawingCanvas from "./components/DrawableCanvas.vue"
+import { ref, onMounted, computed, reactive, inject } from 'vue'
+
+import DrawingCanvas from "./components/DrawableCanvas.vue";
+
+const children = ref([DrawingCanvas, DrawingCanvas]);
+
+const addNewComponent = () => {
+  children.value.push(DrawingCanvas);
+}
 </script>
 
 <template>
   <h1>Håndskriftsgenkendelse</h1>
-  <DrawingCanvas />
-  <DrawingCanvas />
-  <DrawingCanvas />
+  <div>
+    <div v-for="component in children" :key="component.name">
+        <component :is="component"></component>
+    </div>
+  </div>
+  
+  <button @click="addNewComponent()" class="new-line">Tilføj ny linje</button>
 </template>
 
 <style scoped>
-
+button.new-line {
+  margin-left: 50px;
+  padding: 10px;
+}
 </style>
